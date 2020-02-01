333ac8d3260647381ada4ec4d698ee089e250cdd
Lydia B <lydiabaik@gmail.com>
2020-02-01 21:42:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,34 +1,19 @@
-# require 'nokogiri'
-# require 'open-uri'
-#
-# require_relative './course.rb'
-#
-# class Scraper
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
-  # def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title && course.title != ""
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+require_relative './course.rb'
 
+class Scraper
 
-  require 'nokogiri'
-  require 'open-uri'
-  require 'pry'
+  def get_page
 
-  require_relative './course.rb'
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
-  class Scraper
-
-    def get_page
-      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      binding.pry
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
-end
-
-Scraper.new.get_page
