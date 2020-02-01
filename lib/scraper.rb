d5af9191cc2c44c937f7b5fc34bbdfe118528319
Lydia B <lydiabaik@gmail.com>
2020-02-01 21:29:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,21 +1,36 @@
-require 'nokogiri'
-require 'open-uri'
+# require 'nokogiri'
+# require 'open-uri'
+#
+# require_relative './course.rb'
+#
+# class Scraper
 
-require_relative './course.rb'
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
 
-class Scraper
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
+  require 'nokogiri'
+  require 'open-uri'
+  require 'pry'
+
+  require_relative './course.rb'
+
+  class Scraper
+
+    def get_page
+        # more code coming soon!
     end
-  end
 
+  end
+  
   def get_page
   end
 
